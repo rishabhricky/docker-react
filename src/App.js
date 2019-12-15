7c277b3a0558728acf70b719d323329440af9ccd
Rishabh <rishabhrusia22@gmail.com>
2019-12-15 21:39:50 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Checking new1 feature Rishabh, edit <code>src/App.js</code> and save to reload.
+          Hello world feature Rishabh, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
